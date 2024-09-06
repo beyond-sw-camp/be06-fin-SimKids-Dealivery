c95585042e3b3574e33f657256dd0d82e53626e3
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-09-06 11:49:42 +0900

    [Feat] 판매자 사이드 메뉴 컴포넌트 구현 (#69)

---
+++ frontend/src/components/company/CompanyAsideComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="container">
+    <div id="main">
+      <div id="content">
+        <div class="page_aticle aticle_type2">
+          <div id="snb" class="snb_my">
+            <h2 class="tit_snb">판매자 메뉴</h2>
+            <div class="inner_snb">
+              <ul class="list_menu">
+                <li>
+                  <a href="#">게시글 등록</a>
+                </li>
+                <li>
+                  <a href="#">게시글 조회</a>
+                </li>
+                <li>
+                  <a href="#">주문 관리</a>
+                </li>
+                <li>
+                  <a href="#">1:1문의 관리</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+  props: {},
+};
+</script>
+
+<style scoped>
+@import "./css/common.css";
+@import "./css/reset.css";
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,22 @@
 import MainPage from "@/pages/MainPage.vue";
+import CompanyBoardPage from "../pages/company/board/CompanyBoardPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-        path: "/",component: MainPage,
+      path: "/",
+      component: MainPage,
+      children: [
+        {
+          path: "company",
+          component: CompanyBoardPage,
+          meta: { requiresAuth: false },
+        },
+      ],
     },
-    
-  ]
+  ],
 });
 
 export default router;
