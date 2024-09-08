e4630780890b15a1676cf235a31c8d1ab59c607d
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-09-08 18:24:19 +0900

    [Feat] 배너 컴포넌트 구현 (#75)

--- frontend/src/pages/common/MainPage.vue
+++ frontend/src/pages/common/MainPage.vue
@@ -1,5 +1,6 @@
 <template>
   <HeaderComponent></HeaderComponent>
+  <BannerComponent></BannerComponent>
   <router-view></router-view>
   <FooterComponent></FooterComponent>
 </template>
@@ -7,19 +8,19 @@
 <script>
 import HeaderComponent from '@/components/common/HeaderComponent.vue';
 import FooterComponent from '@/components/common/FooterComponent.vue';
+import BannerComponent from '@/components/common/BannerComponent.vue';
 
 export default {
   name: 'MainPage',
   props: {
     msg: String
   },
-  components:{
+  components: {
     HeaderComponent,
+    BannerComponent,
     FooterComponent
   }
 
 }
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
