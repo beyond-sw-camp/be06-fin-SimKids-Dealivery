7bc61801da74359242e50cdcf4b965b73b2c16a3
kangkigs <dlflq11@gmail.com>
2024-09-09 17:51:10 +0900

    [Feat] 일반회원가입 기능 구현 (#72)

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 // const backend = "http://localhost:8080/api/login";
 //로그인 성공 요청 mocky url: https://run.mocky.io/v3/cd7370a5-b1be-41a1-b680-4f93dd1a67bc
+//일반회원가입 성공 요청 mocky url: https://run.mocky.io/v3/86385646-d65e-40fc-9173-b609a688da25
 
 export const useUserStore = defineStore('user', {
     state: () => ({
@@ -24,6 +25,24 @@ export const useUserStore = defineStore('user', {
             }catch(error){
                 alert("로그인에 실패했습니다. 아이디 비밀번호를 확인해주세요.");
             }   
+        },
+
+        async userSignup(sigunupRequest){
+            try{
+                let response = await axios.post("https://run.mocky.io/v3/86385646-d65e-40fc-9173-b609a688da25",sigunupRequest, {withCredentials: true});
+                // let response = await axios.post(backend,signupRequest, {withCredentials: true});
+                console.log(sigunupRequest);
+                console.log(response);
+                if(response.data.code !== 1000){
+                    return false;
+                }else{
+                    return true;
+                }
+                
+                
+            }catch(error){
+                alert("회원가입에 실패했습니다. 입력한 정보를 다시 한 번 확인해주세요.\n\n반복적인 문제 발생시 고객센터로 문의바랍니다.");
+            } 
         }
     }
 
