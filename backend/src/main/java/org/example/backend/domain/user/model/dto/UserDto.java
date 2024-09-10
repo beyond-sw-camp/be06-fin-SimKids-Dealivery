0c8f4f17325a6c11a035412021a9258c3b7246bf
kangkigs <dlflq11@gmail.com>
2024-09-10 20:24:03 +0900

    [Feat] 일반회원가입 기능 구현 (#1)

--- backend/src/main/java/org/example/backend/domain/user/model/dto/UserDto.java
+++ backend/src/main/java/org/example/backend/domain/user/model/dto/UserDto.java
@@ -1,8 +1,77 @@
 package org.example.backend.domain.user.model.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.domain.user.model.entity.User;
+
+import java.time.LocalDateTime;
+
 public class UserDto {
 
-    static class UserSignupRequest{
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class UserSignupRequest{
+        @NotBlank
+        @Size(max = 10, message = "이름은 10자까지 입력 가능합니다.")
+        private String name;
 
+        @NotBlank
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+                message = "아이디는 이메일 형식이어야 합니다.")
+        @Size(max = 40, message = "아이디는 최대 40자까지 입력 가능합니다.")
+        private String email;
+
+        @NotBlank
+        @Pattern(regexp = "^[a-zA-Z0-9]{6}$", message = "인증코드의 형식이 올바르지 않습니다.")
+        private String emailCode;
+
+        @NotBlank
+        @Size(max = 20, message = "비밀번호는 최대 20자까지 입력 가능합니다.")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~!@#])[A-Za-z\\d~!@#]{8,}$",
+        message = "비밀번호는 대문자,소문자,숫자,특수문자 (~!@#) 각각 1개 이상 포함하여 8글자 이상이어야 합니다.")
+        private String password;
+
+        @NotBlank
+        @Pattern(regexp = "^(010)-\\d{4}-\\d{4}$", message = "휴대폰 번호 형식이 올바르지 않습니다. (-를 포함해서 입력해주세요)")
+        private String phoneNumber;
+
+        @NotBlank
+        @Size(max = 6, message = "우편번호는 최대 6자까지 입력 가능합니다.")
+        @Pattern(regexp = "^\\d{5,}$", message = "우편번호 형식이 올바르지 않습니다.")
+        private String postNumber;
+
+        @NotBlank
+        @Size(max = 30, message = "주소는 최대 30자까지 입력 가능합니다.")
+        private String address;
+
+        @NotBlank
+        @Size(max = 30, message = "상세주소는 최대 30자까지 입력 가능합니다.")
+        private String addressDetail;
+
+        @NotBlank
+        private String type;
+
+        public User toEntity(String encodedPassword){
+            return User.builder()
+                    .name(this.name)
+                    .email(this.email)
+                    .password(encodedPassword)
+                    .address(this.address)
+                    .addressDetail(this.addressDetail)
+                    .emailStatus(true)
+                    .status(true)
+                    .role("ROLE_USER")
+                    .phoneNumber(this.phoneNumber)
+                    .postNumber(this.postNumber)
+                    .type(this.type)
+                    .registeredAt(LocalDateTime.now())
+                    .point(0L)
+                    .build();
+        }
     }
 }
--- backend/src/main/java/org/example/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/example/backend/global/exception/GlobalExceptionHandler.java
@@ -1,17 +1,40 @@
 package org.example.backend.global.exception;
 
 
+import jakarta.validation.ConstraintViolationException;
 import org.example.backend.global.common.BaseResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Order(value = 1)
 public class GlobalExceptionHandler {
 
+
+    //커스텀 예외 핸들러(BaseResponse 기반)
     @ExceptionHandler(RuntimeException.class)
     public BaseResponse<String> handleGlobalException(InvalidCustomException e) {
         return new BaseResponse<>(e.getStatus());
+    }
+
+    //@Valid 실패시 예외 핸들러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> error = new HashMap<>();
+        if (!ex.getBindingResult().getAllErrors().isEmpty()) {
+            FieldError errorField = (FieldError) ex.getBindingResult().getAllErrors().get(0);
+            String errorMessage = errorField.getDefaultMessage();
+            error.put("message", errorMessage);
+            error.put("isSuccess", false);
+        }
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
