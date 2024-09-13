7479ca938072977ff928affb60cbe9e34b243438
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-09-13 10:39:42 +0900

    [Fix] 문의 삭제 기능 복구 (#64)

--- frontend/src/components/board/BoardDetailNavComponent.vue
+++ frontend/src/components/board/BoardDetailNavComponent.vue
@@ -197,6 +197,10 @@ export default {
         this.closeModal();
       }
     },
+    deleteInquiry(index) {
+      // 문의를 삭제하는 메소드
+      this.localTableData.splice(index, 1); // 해당 인덱스의 문의 삭제
+    },
   },
   watch: {
     tableData(newData) {
