6b5fdb69ece873cf08498d24c3cfe8a5aa675466
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-09-15 16:40:29 +0900

    [Feat] Question 엔티티 추가 (#42)

--- backend/src/main/java/org/example/backend/domain/qna/model/entity/Question.java
+++ backend/src/main/java/org/example/backend/domain/qna/model/entity/Question.java
@@ -1,4 +1,69 @@
 package org.example.backend.domain.qna.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.domain.board.model.entity.ProductBoard;
+import org.example.backend.domain.user.model.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_board_idx")
+    private ProductBoard productBoard;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @Enumerated(EnumType.STRING)
+    private AnswerStatus answerStatus = AnswerStatus.WAITING;
+
+    // 답변이 등록 되면 문의 상태를 "답변완료"로 변경하는 메서드
+//    public void markAsAnswered(){
+//        this.answerStatus = AnswerStatus.COMPLETED;
+//    }
+
+    public enum AnswerStatus {
+        WAITING("답변대기"),
+        COMPLETED("답변완료");
+
+        private final String value;
+
+        AnswerStatus(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+    }
 }
