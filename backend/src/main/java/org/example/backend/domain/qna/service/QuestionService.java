0416a3c46aefe2dd765971348b62ef07482167a1
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-09-22 17:30:33 +0900

    [Refactor] QuestionService에서 엔티티의 변환 메서드를 호출하여 DTO로 변환하도록 리팩토링 (#195)

--- backend/src/main/java/org/example/backend/domain/qna/service/QuestionService.java
+++ backend/src/main/java/org/example/backend/domain/qna/service/QuestionService.java
@@ -37,30 +37,17 @@ public class QuestionService {
         Question question = request.toEntity(user, productBoard);
         questionRepository.save(question);
 
-        // 문의 등록 후 사용자 이름, 답변 상태, 등록 날짜 함께 반환
-        return QuestionDto.QuestionCreateResponse.builder()
-                .idx(question.getIdx())
-                .title(question.getTitle())
-                .content(question.getContent())
-                .userName(user.getName())  // 사용자 이름 반환
-                .answerStatus(question.getAnswerStatus())  // 답변 상태 반환
-                .createdAt(question.getCreatedAt())  // 생성 날짜 반환
-                .build();
+        // 엔티티의 변환 메서드를 호출하여 DTO 반환
+        return question.toQuestionCreateResponse();
     }
+
     public List<QuestionDto.QuestionListResponse> getQuestions() {
         return questionRepository.findAll().stream()
-                .map(question -> QuestionDto.QuestionListResponse.builder()
-                        .idx(question.getIdx())
-                        .title(question.getTitle())
-                        .content(question.getContent())
-                        .userName(question.getUser().getName())  // 사용자 이름
-                        .answerStatus(question.getAnswerStatus())  // 답변 상태
-                        .createdAt(question.getCreatedAt())  // 생성일
-                        .email(question.getUser().getEmail())
-                        .build())
+                .map(Question::toQuestionListResponse)  // 엔티티의 변환 메서드 사용
                 .collect(Collectors.toList());
     }
-    public void deleteQuestion(Long questionId, String email){
+
+    public void deleteQuestion(Long questionId, String email) {
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new InvalidCustomException(BaseResponseStatus.QNA_ANSWER_DELETE_FAIL_NOT_FOUND));
 
