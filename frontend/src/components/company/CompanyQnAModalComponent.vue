efdc4f4d3a331cc6f129ebee07e160d5be5e8bca
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-09-23 12:04:54 +0900

    [Refactor] 모달창 크기 및 내부 콘텐츠 레이아웃 수정 (#79)

--- frontend/src/components/company/CompanyQnAModalComponent.vue
+++ frontend/src/components/company/CompanyQnAModalComponent.vue
@@ -159,11 +159,11 @@ export default {
 
 .modal-content {
   background-color: #fefefe;
-  margin: 15% auto; /* 상단 여백을 15% 설정하고, 자동으로 좌우 중앙 정렬 */
+  margin: 7% auto; /* 상단 여백을 7% 설정하고, 자동으로 좌우 중앙 정렬 */
   padding: 20px;
   border: 1px solid #888;
   width: 80%; /* 모달의 너비를 80%로 설정 */
-  max-width: 850px;
+  max-width: 680px;
   border-radius: 10px;
   /* max-width: 600px; 최대 너비 설정 */
 }
@@ -302,9 +302,8 @@ textarea {
 .css-190e3ze {
   display: flex;
   flex-direction: column;
-  width: 800px;
-  height: 690px;
-  padding: 30px;
+  width: 600px;
+  padding: 15px;
   background: rgb(255, 255, 255);
 }
 
@@ -461,7 +460,7 @@ img {
   position: relative;
   display: flex;
   flex-direction: column;
-  height: 260px;
+  height: 140px;
   background-color: rgb(255, 255, 255);
   border: 1px solid rgb(221, 221, 221);
   border-radius: 4px;
@@ -479,7 +478,7 @@ img {
 
 .css-f9c7pn button {
   width: 160px;
-  height: 56px;
+  height: 52px;
   border-radius: 3px;
 }
 
