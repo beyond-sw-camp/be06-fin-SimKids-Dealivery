6237015bd6431037cf67b729b306f2f9faab433b
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-10-11 10:36:45 +0900

    [Feat] 진행 큐 추가

--- queue-system/quequeflow/src/main/java/com/example/quequeflow/domain/queue/service/QueueService.java
+++ queue-system/quequeflow/src/main/java/com/example/quequeflow/domain/queue/service/QueueService.java
@@ -1,9 +1,7 @@
 package com.example.quequeflow.domain.queue.service;
 
-import java.time.Duration;
 import java.time.Instant;
 
-import java.util.Set;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +14,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class QueueService {
 	private final RedisTemplate<String, String> redisTemplate;
+	// 대기 큐
 	private final String USER_QUEUE_WAIT_KEY = "queue:wait";
+	// 진행 큐
 	private final String USER_QUEUE_PROCEED_KEY = "queue:proceed";
+	private final int MAX_PROCEED_SIZE = 1;
 
 	//List<> list ; // 식별자 : 만료시간
 
 	// **쿠키값 없고 대기열 등록 안돼있을 때** 대기열 등록하는 메소드
 	public Long registerWaitQueue(final Long boardIdx, final Long userIdx) {
-		String waitQueueKey = getWaitQueueKey(boardIdx);
+		String queueKey = choiceQueue(boardIdx);
 		long unixTimestamp = Instant.now().getEpochSecond();
+
+		// 1. 대기 없이 바로 진입
+		if (queueKey.equals(getProceedQueueKey(boardIdx))) {
+			redisTemplate.opsForZSet().add(queueKey, userIdx.toString(), unixTimestamp);
+			return -1L;
+		}
+
+		// 2. 대기 존재
 		// key: 대기열 키, value: {유저 id, 유닉스 타임 스탬프} 기준으로 Redis에 등록
-		Boolean added = redisTemplate.opsForZSet().add(waitQueueKey, userIdx.toString(), unixTimestamp);
+		Boolean added = redisTemplate.opsForZSet().add(queueKey, userIdx.toString(), unixTimestamp);
 
 		// 이미 등록되어 있으면 예외 발생 시킴 -> registerWaitQueue 호출한 try catch의 catch로 이동
 		if (Boolean.FALSE.equals(added)) {
@@ -34,7 +43,7 @@ public class QueueService {
 		}
 
 		// 현재 순위 반환
-		Long rank = redisTemplate.opsForZSet().rank(waitQueueKey, userIdx.toString());
+		Long rank = redisTemplate.opsForZSet().rank(queueKey, userIdx.toString());
 		return (rank != null && rank >= 0) ? rank + 1 : -1;
 	}
 
@@ -46,7 +55,25 @@ public class QueueService {
 		return (removedCount != null && removedCount > 0);
 	}
 
+	private String choiceQueue(Long boardIdx) {
+		String waitQueueKey = getWaitQueueKey(boardIdx);
+		String proceedQueueKey = getProceedQueueKey(boardIdx);
+		Long waitQueueCount = getCount(waitQueueKey);
+		Long proceedQueueCount = getCount(proceedQueueKey);
 
+		if (waitQueueCount == 0 && proceedQueueCount < MAX_PROCEED_SIZE) {
+			return proceedQueueKey;
+		}
+		return waitQueueKey;
+	}
+
+	private Long getCount(String waitQueueKey) {
+		Long queueCount = redisTemplate.opsForZSet().zCard(waitQueueKey);
+		if (queueCount == null) {
+			return 0L;
+		}
+		return queueCount;
+	}
 
 
 	// 현재 순위 반환
